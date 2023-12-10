185fb8cbac5a957321e72b3bbe12ec168eafc1fd
GiselleBarbosa <g88barbosa@gmail.com>
2023-12-10 14:04:56 -0300

    exemplo 05 - utilizando try-catch

--- index.js
+++ index.js
@@ -1,28 +1,34 @@
 import chalk from "chalk";
-import { error } from 'console';
 import fs from "fs";
 
 function trataErro(erro) {
 	throw new Error(chalk.red(erro.code, "não existe arquivo no diretório"));
 }
 
-/* function pegaArquivo(caminhoDoArquivo){
-const encoding = 'utf-8'
-  fs.readFile(caminhoDoArquivo, encoding, (erro, texto) => {
-    if(erro) {
-      trataErro(erro)
-    }
-    console.log(chalk.green(texto));
-    
-  })
-} */
+// async/await 
+ 
+async function pegaArquivo(caminhoDoArquivo) {
+  try {
+     	const encoding = "utf-8";
+      const texto = await fs.promises
+		.readFile(caminhoDoArquivo, encoding)
 
+    console.log(chalk.green(texto));
+  }
+  catch(erro) {
+    trataErro(erro)
+  }  
+}
+// promises com Then()
+
+/* 
 function pegaArquivo(caminhoDoArquivo) {
 	const encoding = "utf-8";
 	fs.promises
 		.readFile(caminhoDoArquivo, encoding)
 		.then(texto => console.log(chalk.green(texto)))
 		.catch( trataErro());
-}
+} */
 
 pegaArquivo("./arquivos/texto.md");
+pegaArquivo("./arquivos/");
