cbd635f1135a4ce4de768177fddded824556cbb0
GiselleBarbosa <g88barbosa@gmail.com>
2023-12-10 15:36:57 -0300

    exemplo 10 - conectado metodo com arquivo md para buscas dos links

--- index.js
+++ index.js
@@ -1,47 +1,38 @@
 import chalk from "chalk";
 import fs from "fs";
 
+function extraiLinks(texto) {
+	const regex = /\[([^[\]]*?)\]\((http?s:\/\/[^\s?#.].[^\s]*\))/gm;
+	const capturas = [...texto.matchAll(regex)];
+	const resultados = capturas.map(captura => ({ [captura[1]]: [captura[2]] }));
+	return resultados;
+}
+
+// async/await
+async function pegaArquivo(caminhoDoArquivo) {
+	try {
+		const encoding = "utf-8";
+		const texto = await fs.promises.readFile(caminhoDoArquivo, encoding);
+		console.log(extraiLinks(texto));
+	} catch (erro) {
+		trataErro(erro);
+	}
+}
+
 function trataErro(erro) {
 	throw new Error(chalk.red(erro.code, "não existe arquivo no diretório"));
 }
 
-// async/await  
-async function pegaArquivo(caminhoDoArquivo) {
-  try {
-     	const encoding = "utf-8";
-      const texto = await fs.promises
-		.readFile(caminhoDoArquivo, encoding)
-
-    console.log(chalk.green(texto));
-  }
-  catch(erro) {
-    trataErro(erro)
-  }  
-}
-
-// pegaArquivo("./arquivos/texto.md");
-// pegaArquivo("./arquivos/");
-
-
-function extraiLinks(texto){
-  const regex = /\[([^[\]]*?)\]\((http?s:\/\/[^\s?#.].[^\s]*\))/gm;
-  const capturas = regex.exec(texto)
-  console.log(capturas);
-  
-}
-
-const textoTeste = 'São geralmente recuperados a partir de um objeto [FileList](https://developer.mozilla.org/pt-BR/docs/Web/API/FileList) que é retornado como resultado da seleção, pelo usuário, de arquivos através do elemento [<input>](https://developer.mozilla.org/pt-BR/docs/Web/HTML/Element/Input), a partir do objeto [DataTransfer](https://developer.mozilla.org/pt-BR/docs/Web/API/DataTransfer) utilizado em operações de arrastar e soltar, ou a partir da API `mozGetAsFile()` em um [HTMLCanvasElement](https://developer.mozilla.org/pt-BR/docs/Web/API/HTMLCanvasElement). Em Gecko, códigos com privilégiios podem criar objetos File representando qualquer arquivo local sem a intereção do usuário (veja [Implementation notes](https://developer.mozilla.org/pt-BR/docs/Web/API/File#implementation_notes) para mais informações.).'
-
-extraiLinks(textoTeste)
+pegaArquivo("./arquivos/texto.md");
 
 /* \[[^[\]]*?\] */
 
 /* /\(http?s:\/\/[^\s?#.].[^\s]*\) */
 
-//  regex unificado: 
+//  regex unificado:
 // \[[^[\]]*?\]/\(http?s:\/\/[^\s?#.].[^\s]*\)
 
-//  regex finalizado: 
+//  regex finalizado:
 // \[([^[\]]*?)\]\((http?s:\/\/[^\s?#.].[^\s]*\))
 
 // regex apos selecao do flavor verificado pelo site : https://regex101.com/
