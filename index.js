47e12ad581b3c815758d14e7520cd93fe3202b9f
GiselleBarbosa <g88barbosa@gmail.com>
2023-12-10 13:26:35 -0300

    exemplo 03 - adicionado tratamento de erros

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 import chalk from 'chalk';
 import fs from 'fs'
 
+function trataErro(erro) {
+  throw new Error(chalk.red(erro.code, 'não existe arquivo no diretório'))
+}
+
 function pegaArquivo(caminhoDoArquivo){
 const encoding = 'utf-8'
-  fs.readFile(caminhoDoArquivo, encoding, (_, texto) => {
+  fs.readFile(caminhoDoArquivo, encoding, (erro, texto) => {
+    if(erro) {
+      trataErro(erro)
+    }
     console.log(chalk.green(texto));
+    
   })
 }
 
