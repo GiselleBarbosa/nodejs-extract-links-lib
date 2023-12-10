10cebabf7aca9b421105d88132c3cb58806552d6
GiselleBarbosa <g88barbosa@gmail.com>
2023-12-10 13:12:06 -0300

    exemplo 2 - usando a lib FS

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 import chalk from 'chalk';
-import libChalk from 'chalk';
+import fs from 'fs'
 
-console.log('olá mundo!');
+function pegaArquivo(caminhoDoArquivo){
+const encoding = 'utf-8'
+  fs.readFile(caminhoDoArquivo, encoding, (_, texto) => {
+    console.log(chalk.green(texto));
+  })
+}
 
-console.log(chalk.blue('olá mundo em AZUL!'));
-
-console.log(libChalk.blue.bgCyan.bold('Testando Chalks com background Blue'));
-
-console.log(chalk.yellow('São geralmente recuperados a partir de um objeto [FileList](https://developer.mozilla.org/pt-BR/docs/Web/API/FileList) que é retornado como resultado da seleção, pelo usuário, de arquivos através do elemento [<input>](https://developer.mozilla.org/pt-BR/docs/Web/HTML/Element/Input), a partir do objeto [DataTransfer](https://developer.mozilla.org/pt-BR/docs/Web/API/DataTransfer) utilizado em operações de arrastar e soltar, ou a partir da API `mozGetAsFile()` em um [HTMLCanvasElement](https://developer.mozilla.org/pt-BR/docs/Web/API/HTMLCanvasElement). Em Gecko, códigos com privilégiios podem criar objetos File representando qualquer arquivo local sem a intereção do usuário (veja [Implementation notes](https://developer.mozilla.org/pt-BR/docs/Web/API/File#implementation_notes) para mais informações.).'));
+pegaArquivo('./arquivos/texto.md')
