2f6717ed1dd85656b7ba1f0d38f583649d532ece
GiselleBarbosa <g88barbosa@gmail.com>
2023-12-10 15:07:54 -0300

    exemplo 08- add metodo para capturar somente links com o regEx

--- index.js
+++ index.js
@@ -19,9 +19,30 @@ async function pegaArquivo(caminhoDoArquivo) {
   }  
 }
 
-pegaArquivo("./arquivos/texto.md");
-pegaArquivo("./arquivos/");
+// pegaArquivo("./arquivos/texto.md");
+// pegaArquivo("./arquivos/");
+
+
+function extraiLinks(texto){
+  const regex = /\[([^[\]]*?)\]\((http?s:\/\/[^\s?#.].[^\s]*\))/gm;
+  const capturas = regex.exec(texto)
+  console.log(capturas);
+  
+}
+
+const textoTeste = 'São geralmente recuperados a partir de um objeto [FileList](https://developer.mozilla.org/pt-BR/docs/Web/API/FileList) que é retornado como resultado da seleção, pelo usuário, de arquivos através do elemento [<input>](https://developer.mozilla.org/pt-BR/docs/Web/HTML/Element/Input), a partir do objeto [DataTransfer](https://developer.mozilla.org/pt-BR/docs/Web/API/DataTransfer) utilizado em operações de arrastar e soltar, ou a partir da API `mozGetAsFile()` em um [HTMLCanvasElement](https://developer.mozilla.org/pt-BR/docs/Web/API/HTMLCanvasElement). Em Gecko, códigos com privilégiios podem criar objetos File representando qualquer arquivo local sem a intereção do usuário (veja [Implementation notes](https://developer.mozilla.org/pt-BR/docs/Web/API/File#implementation_notes) para mais informações.).'
+
+extraiLinks(textoTeste)
 
 /* \[[^[\]]*?\] */
 
-/* /\(http?s:\/\/[^\s?#.].[^\s]*\)/gm */
+/* /\(http?s:\/\/[^\s?#.].[^\s]*\) */
+
+//  regex unificado: 
+// \[[^[\]]*?\]/\(http?s:\/\/[^\s?#.].[^\s]*\)
+
+//  regex finalizado: 
+// \[([^[\]]*?)\]\((http?s:\/\/[^\s?#.].[^\s]*\))
+
+// regex apos selecao do flavor verificado pelo site : https://regex101.com/
+// /\[([^[\]]*?)\]\((http?s:\/\/[^\s?#.].[^\s]*\))/gm;
