569059abb85797cf6cd66a108c9b0de5602c4db8
GiselleBarbosa <g88barbosa@gmail.com>
2023-12-10 22:19:37 -0300

    fix: corrigido regex e adicionado metodo para tratar o texto

--- src/index.js
+++ src/index.js
@@ -2,17 +2,20 @@ import chalk from "chalk";
 import fs from "fs";
 
 function extraiLinks(texto) {
-	const regex = /\[([^[\]]*?)\]\((http?s:\/\/[^\s?#.].[^\s]*\))/gm;
+	const regex = /\[([^[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)/gm;
+
 	const capturas = [...texto.matchAll(regex)];
-	const resultados = capturas.map(captura => ({ [captura[1]]: [captura[2]] }));
-	return resultados;
+	const resultados = capturas.map(captura => ({ [captura[1]]: captura[2] }));
+	return resultados.length !== 0
+		? resultados
+		: "Não foram encontrados links no arquivo. ";
 }
 
 async function pegaArquivo(caminhoDoArquivo) {
 	try {
 		const encoding = "utf-8";
 		const texto = await fs.promises.readFile(caminhoDoArquivo, encoding);
-		console.log(extraiLinks(texto));
+		return extraiLinks(texto);
 	} catch (erro) {
 		trataErro(erro);
 	}
@@ -21,6 +24,5 @@ async function pegaArquivo(caminhoDoArquivo) {
 function trataErro(erro) {
 	throw new Error(chalk.red(erro.code, "não existe arquivo no diretório"));
 }
-
 
 export default pegaArquivo;
